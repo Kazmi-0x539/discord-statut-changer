dca94bedbad61ecc60a442ffb0245ac18273c16c
Kazmi_1337 <81714861+Kazmi-0x539@users.noreply.github.com>
2021-04-26 01:12:00 +0000

    Create statutchanger.py

---
+++ statutchanger.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.members = True
+
+import random
+import asyncio
+
+bot = commands.Bot(command_prefix="!",help_command=None, intents=intents)
+
+memberss = []
+
+## CONFIG ##
+
+token = ""
+
+delay = 5
+
+MessageBeforeTheNameOfMember = "Hey "
+
+## CONFIG END
+
+
+@bot.event
+async def on_ready():
+
+    print("Bot connected !")
+
+    global memberss
+
+    for guild in bot.guilds:
+        for member in guild.members:
+            memberss.append(member)
+
+        while True:
+            
+            memberchoose = random.choice(memberss)
+            await bot.change_presence(activity=discord.Game(name=str(MessageBeforeTheNameOfMember)+str(memberchoose)))
+            await asyncio.sleep(delay)
+
+
+
+    
+
+bot.run(str(token))
